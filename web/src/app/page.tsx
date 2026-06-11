3e2b16bae84a88774f28ad7cc850ca0ebdaf33f3
0x-IHRR <wzx824030716@gmail.com>
2026-06-11 16:44:48 +0800

    feat: implement continuous homepage canvas

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,21 +1,5 @@
-import { BrandFilmSection } from "@/components/brand-film-section";
-import { HomeEntrances } from "@/components/home-entrances";
-import { HomeHero } from "@/components/home-hero";
-import { QuietClosing } from "@/components/quiet-closing";
-import { SiteFooter } from "@/components/site-footer";
-import { SiteHeader } from "@/components/site-header";
+import { ContinuousHomepage } from "@/components/continuous-homepage";
 
 export default function Home() {
-  return (
-    <div className="page-ambient">
-      <SiteHeader />
-      <main className="flex flex-1 flex-col">
-        <HomeHero />
-        <BrandFilmSection />
-        <HomeEntrances />
-        <QuietClosing />
-      </main>
-      <SiteFooter />
-    </div>
-  );
+  return <ContinuousHomepage />;
 }
